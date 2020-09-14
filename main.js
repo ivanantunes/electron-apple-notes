7e99e2ab718f7f3eb4e221327de861ab75bc8a8e
Ivan Antunes <ivanantnes75@gmail.com>
2020-09-14 10:00:14 -0300

    base projeto

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            preload: `${__dirname}/preload.js`,
+        },
+    });
+
+    mainWindow.removeMenu();
+
+    mainWindow.loadURL('https://www.icloud.com/notes');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', () => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
